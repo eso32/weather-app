e291c54481484bafcf8d50ff9102a1e6c087b691
Tomek <tomek.szlapa@gmail.com>
2017-07-29 10:04:22 +0200

    Fix search by cutting off white spaces. Add background photo for predefined widgets. Add favicon and social buttons at bottom.

--- js/app.js
+++ js/app.js
@@ -1,101 +1,134 @@
+//Budowa 7 predefiniowanych widgetów
 $(document).ready(function(){
-  var jqxhr, a
-      url = "proxy.php",
+  var jqxhr,
+      a = "",
       $fixedWidgets = $('.widget'),
-      woeIDs = [2488853, 2488042, 2455920, 2487956, 23511744, 523920, 615702];
+      woeIDs = [2459115, 753692, 721943, 2487956, 638242, 523920, 615702];
 
   $.each($fixedWidgets, function(i, item) {
-    jqxhr = $.getJSON( url, {c: woeIDs[i]} ,function(response){
+    //Pobranie danych z API
+    jqxhr = $.getJSON( "proxy.php", {c: woeIDs[i]} ,function(response){
       var data = response.consolidated_weather;
-      a =
-                '<div class="row"><div class="col-md-12">'+
-                  '<h3>'+response.title+'</h3><span class="weather">'+data[0].weather_state_name+'</span><br/>'+
-                  '<span class="temp">'+Math.round(data[0].the_temp)+'&#176;C</span>'+
-                  '<img src="https://www.metaweather.com/static/img/weather/'+data[0].weather_state_abbr+'.svg" alt="">'+
-                '</div></div><div class="row widget-bottom">';
+
+      //Wypełnienie danych dot. aktualnego dnia(div.row.widget)
+      a = '<div class="row">' +
+            '<div class="col-md-12">' +
+              '<h3>'+response.title+'</h3><span class="weather">'+data[0].weather_state_name+'</span><br/>' +
+              '<span class="temp">'+Math.round(data[0].the_temp)+'&#176;C</span>' +
+              '<img src="https://www.metaweather.com/static/img/weather/'+data[0].weather_state_abbr+'.svg" alt="Weather state">' +
+            '</div>' +
+          '</div>' +
+          '<div class="row widget-bottom">';
+
+      //Prognoza dla 4 kolejnych dni (div.row.widget-bottom)
       for(i=1; i<=4; i++){
-        a +=
-        '<div class="col-xs-3">'+
-        '<span class="date">'+getWeekday(i)+'</span>'+
-        '<span class="temp-bottom">'+Math.round(data[i].the_temp)+'&#176;C<br/></span>'+
-        '<img src="https://www.metaweather.com/static/img/weather/'+data[i].weather_state_abbr+'.svg" alt="">'+
-        '</div>';
+        a +='<div class="col-xs-3">' +
+              '<span class="date">'+getWeekday(i)+'</span>' +
+              '<span class="temp-bottom">'+Math.round(data[i].the_temp)+'&#176;C<br/></span>'+
+              '<img src="https://www.metaweather.com/static/img/weather/'+data[i].weather_state_abbr+'.svg" alt="">'+
+            '</div>';
       }
       item.innerHTML = a + '</div>';
     });
-  })
+  });
 });
 
+//Przypisanie dnia tygodnia do cyfry kolejnego dnia
 function getWeekday(i){
-  var d = new Date();
-  var weekday = new Array(7);
+  var d = new Date(),
+      day = d.getDay()+i,
+      weekday = ["Sunday", "Monday",
+                "Tuesday", "Wednesday", "Thursday",
+                "Friday", "Saturday"];
 
-  var day = d.getDay()+i;
-
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
   if(day>6){
     day=day - 6;
   }
+
   return weekday[day];
 }
 
+//Wyszukanie miasta na podstawie nazwy - metoda API
 $('#search').on('click', function(){
-  $('#result').html('<div class="loader"><div class="spinner"></div></div>');
-  $('#photoModal').modal('show');
-  $( this ).prop("disabled", true).css('opacity', 0.3);
-  var city = encodeURI($('#city').val());
-  var jqxhr = $.getJSON( "proxy.php", {city: city} ,function(response){
-    if(response.length > 0){
-      $('#result').html('');
-      $.each(response, function(i, item){
-        $( '#result' ).append('<div class="item-found">' + item.title + '<p>' +
-        item.woeid+ '</p></div>');
-      });
-    } else {
-      $('#result').html('');
-      $( '#result' ).append("Nic nie znaleziono :(");
-    }
-  })
-    .done(function() {
-      $( '#search' ).prop("disabled", false).css('opacity', 1);
+
+  var city = $.trim($('#city').val()); //Pobranie wartości oraz usunięcie białych znaków na początku i końcu
+  if(city != ''){
+    var $result = $('#result');
+    $result.html('<div class="loader"><div class="spinner"></div></div>');
+    $('#photoModal').modal('show');
+    $( this ).prop("disabled", true).css('opacity', 0.3);
+
+    var city_encoded = encodeURI(city);
+    var jqxhr = $.getJSON( "proxy.php", {city: city_encoded} ,function(response){
+      if(response.length > 0){
+        $result.html('');
+        $.each(response, function(i, item){
+          $result.append('<div class="item-found">' + item.title + '<p>' +
+          item.woeid+ '</p></div>');
+        });
+      } else {
+        $result.html('');
+        $result.append("Nic nie znaleziono :(");
+      }
     })
-    .fail(function() {
-    });
+      .done(function() {
+        $( '#search' ).prop("disabled", false).css('opacity', 1);
+      })
+      .fail(function() {
+        console.log('Something went wrogn with searchin API: ' + jqxhr.responseText);
+      });
+  } else {
+    $('.notification').toggleClass(' active');
+    setTimeout(function(){ $('.notification').toggleClass(' active'); }, 3000);
+  }
 });
 
+//Pobranie danych API po wyborze miasta w modal oraz dodanie widgetu (nowego miasta)
 $('#result').on('click', 'div.item-found', function(){
-  var woeid = $( this ).children('p').text();
-  $('#result').html('<div class="loader"><div class="spinner"></div></div>');
+  var woeid = $( this ).children('p').text(),
+      $result = $('#result'),
+      a = "";
+
+  //Loader przed wczytaniem listy miast odpowiadających zapytaniu
+  $result.html('<div class="loader"><div class="spinner"></div></div>');
+
   var jqxhr = $.getJSON( "proxy.php", {c: woeid} ,function(response){
         var data = response.consolidated_weather;
-        a =      '<div class="col-md-4"><div class="widget">'+
-                  '<div class="row"><div class="col-md-12">'+
-                    '<h3>'+response.title+'</h3><span class="weather">'+data[0].weather_state_name+'</span><br/>'+
-                    '<span class="temp">'+Math.round(data[0].the_temp)+'&#176;C</span>'+
-                    '<img src="https://www.metaweather.com/static/img/weather/'+data[0].weather_state_abbr+'.svg" alt="">'+
-                  '</div></div><div class="row widget-bottom">';
+
+        //Wypełnienie danych dot. aktualnego dnia(div.row.widget)
+        a =
+        '<div class="col-md-4">' +
+          '<div class="widget">' +
+            '<div class="row">' +
+              '<div class="col-md-12">' +
+                '<h3>'+response.title+'</h3><span class="weather">'+data[0].weather_state_name+'</span><br/>'+
+                '<span class="temp">'+Math.round(data[0].the_temp)+'&#176;C</span>'+
+                '<img src="https://www.metaweather.com/static/img/weather/'+data[0].weather_state_abbr+'.svg" alt="Weather state">'+
+              '</div>' +
+            '</div>' +
+            '<div class="row widget-bottom">';
+
+        //Prognoza dla 4 kolejnych dni (div.row.widget-bottom)
         for(i=1; i<=4; i++){
           a +=
-          '<div class="col-xs-3">'+
-          '<span class="date">'+getWeekday(i)+'</span>'+
-          '<span class="temp-bottom">'+Math.round(data[i].the_temp)+'&#176;C<br/></span>'+
-          '<img src="https://www.metaweather.com/static/img/weather/'+data[i].weather_state_abbr+'.svg" alt="">'+
-          '</div>';
+              '<div class="col-xs-3">'+
+                '<span class="date">'+getWeekday(i)+'</span>'+
+                '<span class="temp-bottom">'+Math.round(data[i].the_temp)+'&#176;C<br/></span>'+
+                '<img src="https://www.metaweather.com/static/img/weather/'+data[i].weather_state_abbr+'.svg" alt="">'+
+              '</div>';
         }
         $('#new').before(a + '</div></div></div>');
         $('#photoModal').modal('hide');
+      })
+      .fail(function() {
+        console.log('Something went wrong: ' + jqxhr.status);
       });
-  $('#result').html("");
-  $('#result').html('<div class="loader"><div class="spinner"></div></div>');
+
+  //Loader po wyborze miasta - pobieranie danych z API
+  $result.html('<div class="loader"><div class="spinner"></div></div>');
 });
 
-//smoothscroll
+//smoothscroll - CSSTricks
 $('a[href*="#"]')
   .not('[href="#"]')
   .not('[href="#0"]')
@@ -125,12 +158,13 @@ $('a[href*="#"]')
           } else {
             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
-        };
+        }
       });
     }
   }
 });
 
+//Rozwinięcie/zwinięcie menu dla okna mniejszego od 900px
 function menu() {
   $( ".hamburger" ).click(function() {
     $( ".menu" ).slideToggle( "slow", function() {
@@ -145,16 +179,15 @@ function menu() {
     });
   });
   refreshMenu();
-};
+}
 
+//Odświeżenie menu po zmianie szerokości okna
 function refreshMenu(){
   if(window.innerWidth<900){
-    console.log('lol');
     $( ".hamburger" ).show();
     $( ".cross" ).hide();
     $( ".menu" ).hide();
   }else {
-    console.log('lol2');
     $( ".menu" ).show();
     $( ".hamburger" ).hide();
     $( ".cross" ).hide();
